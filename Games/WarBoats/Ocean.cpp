72da8d56e858b1e3a9352de0314cb0671e4befb7
PowerUser64 <blake@dit1978us.DIGIPEN.EDU>
2019-09-17 15:08:06 -0700

    Add War boats functions

--- Games/WarBoats/Ocean.cpp
+++ Games/WarBoats/Ocean.cpp
@@ -5,19 +5,78 @@
 
 namespace CS175
 {
-    namespace WarBoats
-    {
-        const int BOAT_LENGTH = 4;  // Length of a boat
-        const int HIT_OFFSET = 100; // Add this to the boat ID
+namespace WarBoats
+{
 
+const int BOAT_LENGTH = 4;  // Length of a boat
+const int HIT_OFFSET = 100; // Add this to the boat ID
 
-        /*
+/*
         * ALL STUDENT IMPLEMENTATION GOES HERE
         */
-        
-        
-        /*********************************************************************/
-        /*!
+
+// Builds an ocean based on a specified number of boats, width and height
+Ocean *CreateOcean(int num_boats, int x_quadrants, int y_quadrants)
+{
+    Ocean *ocean = new Ocean;
+    ocean->grid = new int *[y_quadrants];
+    int j = 0;
+    for (size_t i = 0; i < y_quadrants; i++)
+    {
+        ocean->grid[i] = new int[x_quadrants];
+        for (j = 0; j < x_quadrants; ocean->grid[i][j++] = 0)
+            ;
+    }
+    return ocean;
+}
+
+// deallocates the ocean array
+void DestroyOcean(Ocean *theOcean)
+{
+    for (int i = 0; i < theOcean->y_quadrants; ++i)
+    {
+        delete[] theOcean->grid[i];
+    }
+    delete[] theOcean->grid;
+    delete theOcean;
+}
+
+// takes shots and applies damage to the ocean
+ShotResult TakeShot(Ocean &ocean, const Point &coordinate)
+{
+    // check if it's in bounds
+    if (coordinate.x > ocean.x_quadrants || coordinate.x < 0)
+        return srILLEGAL;
+    if (coordinate.y > ocean.y_quadrants || coordinate.y < 0)
+        return srILLEGAL;
+    // check if it's on the water or on a boat
+    if (ocean.grid[coordinate.x][coordinate.y] == 0)
+    {
+        ocean.grid[coordinate.x][coordinate.y]--;
+        ocean.stats.misses++;
+    }
+    else if (ocean.grid[coordinate.x][coordinate.y] > 0)
+    {
+        ocean.grid[coordinate.x][coordinate.y] += 100;
+        ocean.stats.hits++;
+    }
+}
+
+// asks the user where to put boats
+BoatPlacement PlaceBoat(Ocean &ocean, const Boat &boat)
+{
+    // check for verticle oreintation
+        if (boat.orientation == oHORIZONTAL);
+}
+
+// says wether someone has won
+ShotStats GetShotStats(const Ocean &ocean)
+{
+    return;
+}
+
+/*********************************************************************/
+/*!
         \brief
         Prints the grid (ocean) to the screen.
 
@@ -34,36 +93,36 @@ namespace CS175
         \param showboats
         If true, the boats are shown in the output. (Debugging feature)
         */
-        /*********************************************************************/
-        void DumpOcean(const CS175::WarBoats::Ocean &ocean,
-            int field_width,
-            bool extraline,
-            bool showboats)
+/*********************************************************************/
+void DumpOcean(const CS175::WarBoats::Ocean &ocean,
+               int field_width,
+               bool extraline,
+               bool showboats)
+{
+    // For each row
+    for (int y = 0; y < ocean.y_quadrants; y++)
+    {
+        // For each column
+        for (int x = 0; x < ocean.x_quadrants; x++)
         {
-            // For each row
-            for (int y = 0; y < ocean.y_quadrants; y++)
+            int value = ocean.grid[y * ocean.x_quadrants + x];
+
+            // Is it a boat?
+            if ((value > 0) && (value < HIT_OFFSET) &&
+                (showboats == false))
             {
-                // For each column
-                for (int x = 0; x < ocean.x_quadrants; x++)
-                {
-                    int value = ocean.grid[y * ocean.x_quadrants + x];
-
-                    // Is it a boat?
-                    if ((value > 0) && (value < HIT_OFFSET) && 
-                        (showboats == false))
-                    {
-                        value = 0;
-                    }
-
-                    std::cout << std::setw(field_width) << value;
-                }
-                std::cout << std::endl;
-                if (extraline)
-                {
-                    std::cout << std::endl;
-                }
+                value = 0;
             }
-        }
 
-    } // namespace WarBoats
+            std::cout << std::setw(field_width) << value;
+        }
+        std::cout << std::endl;
+        if (extraline)
+        {
+            std::cout << std::endl;
+        }
+    }
+}
+
+} // namespace WarBoats
 } // namespace CS175
