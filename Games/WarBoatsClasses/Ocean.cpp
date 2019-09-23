7f18693246571066664c6d22935b9a75eb2fd492
PowerUser64 <blake@dit1978us.DIGIPEN.EDU>
2019-09-23 14:48:41 -0700

    Add all my function declarations with comments

--- Games/WarBoatsClasses/Ocean.cpp
+++ Games/WarBoatsClasses/Ocean.cpp
@@ -3,24 +3,74 @@
 
 namespace CS175
 {
-    namespace WarBoats
-    {  
-        /*
-        * ALL STUDENT IMPLEMENTATION GOES HERE
-        */
+namespace WarBoats
+{
+// My functions :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-        const int *Ocean::GetGrid(void) const
-        {
-            return grid;
-        }
-  
-        Point Ocean::GetDimensions(void) const
-        {
-            Point pt = {x_quadrants, y_quadrants};
-            return pt;
-        }
+/* Constructor - The client calls this to create an ocean(game board)
+ * Client specifies the dimensions of the ocean and the number of boats that will be placed in it
+ * All private fields are initialized
+ * No return value
+ */
+Ocean::Ocean(int num_boats_, int x_quadrants_, int y_quadrants_)
+{
+    return;
+}
 
-    } // namespace WarBoats
+/* Destructor-This method is responsible for deallocating anything that was allocated in the constructor
+ * No return value
+ */
+Ocean::~Ocean(void)
+{
+    return;
+}
+
+/* The client calls this to place boats in the ocean
+ * The client will create a boat and pass it to the method
+ * The method must ensure that the boat will "fit" in the ocean
+ * Do this by checking the location where it is to be placed, the orientation (horizontal/vertical),
+ * and whether or not it will overlap with another boat or stick outside of the ocean
+ * The return value indicates whether or not the boat could be placed in the ocean
+ */
+BoatPlacement Ocean::PlaceBoat(const Boat &boat_)
+{
+    return;
+}
+
+/* Client calls this in an attempt to hit one of the boats
+ * The coordinate parameter indicates where the client is attempting to strike
+ * 
+ * There are several possible results : Hit, Miss, Sunk, Duplicate, or Illegal
+ * Hit, Miss, and Duplicate are obvious
+ * Sunk is returned when a shot hits the last undamaged part of a boat
+ * Sunk implies Hit
+ * Illegal is any coordinate that is outside of the ocean(e.g. x / y less than 0 or outside the range)
+ */
+ShotResult Ocean::TakeShot(const Point &coordinate_)
+{
+    return;
+}
+
+/* Returns data about the board that has bneen collected over time
+ */
+ShotStats &Ocean::GetShotStats(void)
+{
+    return;
+}
+
+// End my functions :::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+const int *Ocean::GetGrid(void) const
+{
+    return grid;
+}
+
+Point Ocean::GetDimensions(void) const
+{
+    Point pt = {x_quadrants, y_quadrants};
+    return pt;
+}
+
+} // namespace WarBoats
 
 } // namespace CS175
-
--- Games/WarBoatsClasses/Ocean.h
+++ Games/WarBoatsClasses/Ocean.h
@@ -4,34 +4,69 @@
 
 namespace CS175
 {
-  namespace WarBoats
-  {
-    const int BOAT_LENGTH = 4;  // Length of a boat
-    const int HIT_OFFSET = 100; // Add this to the boat ID
+namespace WarBoats
+{
+const int BOAT_LENGTH = 4;  // Length of a boat
+const int HIT_OFFSET = 100; // Add this to the boat ID
 
-    class Ocean
-    {
-      public:
-        
-          /*
-           * Other public methods
-          */
-        
-           // Provided
-        const int *GetGrid(void) const;
-        Point GetDimensions(void) const;
+class Ocean
+{
+public:
+  // My functions :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-      private:
-        int *grid;        // The 2D ocean
-        int x_quadrants;  // Ocean size along x-axis
-        int y_quadrants;  // Ocean size along y-axis
-        
-        /*
+  /* Constructor - The client calls this to create an ocean(game board)
+   * Client specifies the dimensions of the ocean and the number of boats that will be placed in it
+   * All private fields are initialized
+   * No return value
+   */
+  Ocean(int num_boats_, int x_quadrants_, int y_quadrants_);
+
+  /* Destructor-This method is responsible for deallocating anything that was allocated in the constructor
+   * No return value
+   */
+  ~Ocean(void);
+
+  /* The client calls this to place boats in the ocean
+   * The client will create a boat and pass it to the method
+   * The method must ensure that the boat will "fit" in the ocean
+   * Do this by checking the location where it is to be placed, the orientation (horizontal/vertical),
+   * and whether or not it will overlap with another boat or stick outside of the ocean
+   * The return value indicates whether or not the boat could be placed in the ocean
+   */
+  BoatPlacement PlaceBoat(const Boat &boat_);
+
+  /* Client calls this in an attempt to hit one of the boats
+   * The coordinate parameter indicates where the client is attempting to strike
+   * 
+   * There are several possible results : Hit, Miss, Sunk, Duplicate, or Illegal
+   * Hit, Miss, and Duplicate are obvious
+   * Sunk is returned when a shot hits the last undamaged part of a boat
+   * Sunk implies Hit
+   * Illegal is any coordinate that is outside of the ocean(e.g. x / y less than 0 or outside the range)
+   */
+  ShotResult TakeShot(const Point &coordinate_);
+  
+  /* Returns data about the board that has bneen collected over time
+   */
+  ShotStats &GetShotStats(void);
+
+  // End my functions :::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+  // Provided
+  const int *GetGrid(void) const;
+  Point GetDimensions(void) const;
+
+private:
+  int *grid;      // The 2D ocean
+  int x_quadrants; // Ocean size along x-axis
+  int y_quadrants; // Ocean size along y-axis
+
+  /*
          * Other private data
         */
-        
-    }; // class Ocean
 
-  } // namespace WarBoats
+}; // class Ocean
+
+} // namespace WarBoats
 
 } // namespace CS175
