258805ea285c32e12bc4d12310fd89f5f51b29a5
PowerUser64 <blake@dit1978us.DIGIPEN.EDU>
2019-09-25 15:03:24 -0700

    add math things

---
+++ WarmUp/2019/25/Math-1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cmath>
+
+#define pi M_PIf64
+
+int main()
+{
+    // 2b- ✅
+    std::cout << " 2b- "
+              << 27.0f / 180.0f * pi
+              << std::endl;
+    // 3d- ✅
+    std::cout << " 3d- "
+              << (318.0f / 180.0f) * pi
+              << std::endl;
+    // 5c-
+    std::cout << " 5c- "
+              << (7.0f / 36.0f) * pi
+              << std::endl;
+    // 6e-
+    std::cout << " 6e- "
+              << (5.0f / 3.0f) * pi
+              << std::endl;
+    // 10a-
+    std::cout << "10a- "
+              << sin(215.0f) // 90 120 135 150 180 210
+              << std::endl;
+
+    // 12f-
+    std::cout << "12f- "
+              << 1
+              << std::endl;
+
+    // 14-
+    std::cout << "14 - "
+              << 1
+              << std::endl;
+
+    // 18f-
+    std::cout << "18f- "
+              << 1
+              << std::endl;
+
+    // 20-
+    std::cout << "20 - "
+              << 1
+              << std::endl;
+
+    // 33a-
+    std::cout << "33a- "
+              << 1
+              << std::endl;
+}
