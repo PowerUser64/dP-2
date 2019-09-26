733fdc38babec282240fac5d580df879131fba8f
PowerUser64 <blake@dit1978us.DIGIPEN.EDU>
2019-09-26 15:01:43 -0700

    add more problem answers

--- WarmUp/2019/25/Math-1.cpp
+++ WarmUp/2019/25/Math-1.cpp
@@ -2,52 +2,113 @@
 #include <cmath>
 
 #define pi M_PIf64
+#define filler "💢" // "🌠" "🐱‍🐉"
 
 int main()
 {
-    // 2b- ✅
-    std::cout << " 2b- "
-              << 27.0f / 180.0f * pi
-              << std::endl;
-    // 3d- ✅
-    std::cout << " 3d- "
-              << (318.0f / 180.0f) * pi
-              << std::endl;
-    // 5c-
-    std::cout << " 5c- "
-              << (7.0f / 36.0f) * pi
-              << std::endl;
-    // 6e-
-    std::cout << " 6e- "
-              << (5.0f / 3.0f) * pi
-              << std::endl;
-    // 10a-
-    std::cout << "10a- "
-              << sin(215.0f) // 90 120 135 150 180 210
-              << std::endl;
+    std::cout
 
-    // 12f-
-    std::cout << "12f- "
-              << 1
-              << std::endl;
+        // 2b- ✅
+        << " 2b- "
+        << 27.0f / 180.0f * pi
+        << std::endl
 
-    // 14-
-    std::cout << "14 - "
-              << 1
-              << std::endl;
+        // 3d- ✅
+        << " 3d- "
+        << (318.0f / 180.0f) * pi
+        << std::endl
 
-    // 18f-
-    std::cout << "18f- "
-              << 1
-              << std::endl;
+        // 5c- ✅
+        << " 5c- "
+        << (7.0f / 36.0f) * pi
+        << std::endl
 
-    // 20-
-    std::cout << "20 - "
-              << 1
-              << std::endl;
+        // 6e- ✅
+        << " 6e- "
+        << (5.0f / 3.0f) * pi
+        << std::endl
 
-    // 33a-
-    std::cout << "33a- "
-              << 1
-              << std::endl;
+        // 10a- ✅
+        << "10a- "         // 1  3/2 1/2 1/2  0  1/2 1/2
+        <<  sin(225.0f) - (1/sqrt(2)) // 90 120 135 150 180 210 225
+        << std::endl
+
+        // 12f- ❌
+        << "12f- "
+        << (tan((13.0f / 4.0f) * pi) + 1.0f)
+        << std::endl
+
+        // 14-
+        << "14 ~ "
+        << std::endl
+            // a-
+            << "  a- "
+            << 0 - sqrt(1 - (cos(-0.8))
+            << std::endl
+            // b-
+            << "  b- "
+            << filler
+            << std::endl
+            // c-
+            << "  c- "
+            << filler
+            << std::endl
+            // d-
+            << "  d- "
+            << filler
+            << std::endl
+            // e-
+            << "  e- "
+            << filler
+            << std::endl
+            // f-
+            << "  f- "
+            << filler
+            << std::endl
+            // g-
+            << "  g- "
+            << atan2()
+            << std::endl
+
+        // 18f-
+        << "18f- "
+        << filler
+        << std::endl
+
+        // 20-
+        << "20 ~ "
+        << std::endl
+            // a-
+            << "  a- "
+            << filler
+            << std::endl
+            // b-
+            << "  b- "
+            << filler
+            << std::endl
+            // c-
+            << "  c- "
+            << filler
+            << std::endl
+            // d-
+            << "  d- "
+            << filler
+            << std::endl
+            // e-
+            << "  e- "
+            << filler
+            << std::endl
+            // f-
+            << "  f- "
+            << filler
+            << std::endl
+            // g-
+            << "  g- "
+            << filler
+            << std::endl
+
+        // 33a-
+        << "33a- "
+        << filler
+        << std::endl;
 }
