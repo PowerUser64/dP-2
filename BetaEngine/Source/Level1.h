3465553fbc3dbe0b7ab60f2a6a53f1a0065f125f
PowerUser64 <blakelysnorth@gmail.com>
2019-10-01 15:09:55 -0700

    Finish project

--- BetaEngine/Source/Level1.h
+++ BetaEngine/Source/Level1.h
@@ -5,9 +5,17 @@ class Level1 : public Beta::BetaObject
 {
 public:
 	Level1();
-
-
 	void Initialize() override;
 	void Update(float dt) override;
 	void Shutdown() override;
+
+private:
+	// half speed
+	float rad_pos1 = 0;
+	// normal speed
+	float rad_pos2 = 0;
+	// double speed
+	float rad_pos3 = 0;
+	//cool mode
+	bool cool_mode = false;
 };
