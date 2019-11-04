38bf4d8986d68be46283a56a8f813460fe96f431
PowerUser64 <blakelysnorth@gmail.com>
2019-11-04 14:59:13 -0800

    Add functions decs

--- BetaEngine/Comp/Lab1/Source/MonkeyMovement.cpp
+++ BetaEngine/Comp/Lab1/Source/MonkeyMovement.cpp
@@ -5,7 +5,7 @@
 // Project:		BetaFramework
 // Course:		WANIC VGP2 2018-2019
 //
-// Copyright © 2018 DigiPen (USA) Corporation.
+// Copyright ï¿½ 2018 DigiPen (USA) Corporation.
 //
 //------------------------------------------------------------------------------
 
@@ -15,8 +15,43 @@
 using namespace Beta;
 
 // STUDENT CODE GOES HERE
+/*  monkeyWalkSpeed(3.0f) 
+ *  monkeyJumpSpeed(8.0f) 
+ *  groundHeight(-1.5f) 
+ *  gravity(0.0f, -12.0f)
+ */
 
+// Constructor
+MonkeyMovement()
+{
 
+}
+
+// Initialize this component (happens at object creation).
+void Initialize() override
+{
+
+}
+
+// Fixed update function for this component.
+// Params:
+//   dt = The (fixed) change in time since the last step.
+void Update(float dt) override
+{
+
+}
+
+// Moves horizontally based on input
+void MoveHorizontal() const
+{
+
+}
+
+// Moves vertically based on input
+void MoveVertical() const
+{
+
+}
 
 // Create extra component functions - DO NOT REMOVE
 COMPONENT_SUBCLASS_DEFINITION(MonkeyMovement)
