69a3ef9381518f02a6e7433345c2dd2c8aa18ee5
PowerUser64 <blakelysnorth@gmail.com>
2019-12-16 15:08:21 -0800

    I don't even know

--- BetaEngine/Comp/Lab4/Source/Animation.cpp
+++ BetaEngine/Comp/Lab4/Source/Animation.cpp
@@ -12,11 +12,11 @@
 Animation::Animation(const std::string &_name, const SpriteSource *_spriteSource, unsigned _frameCount, unsigned _frameStart, float _frameDuration)
 {
 	// Time accumulated between last call to GetCurrentFrameIndex and current call
-	//mutable float 
+	//mutable float
 	accumulator = 0;
 
 	// Current frame of the animation (from 0 to frameCount)
-	//mutable unsigned 
+	//mutable unsigned
 	currentVirtualFrame = 0;
 
 	// The amount of time to display each frame.
@@ -24,7 +24,7 @@ Animation::Animation(const std::string &_name, const SpriteSource *_spriteSource
 	frameDuration = _frameDuration;
 
 	// Animation attributes
-	//unsigned 
+	//unsigned
 	frameCount = _frameCount;
 	//unsigned
 	frameStart = _frameStart;
@@ -47,8 +47,25 @@ Animation::Animation(const std::string &_name, const SpriteSource *_spriteSource
  *  Returns:
  *    The frame in the sprite source for the current frame of the animation.
  */
+/* 2.  GetCurrentFrameIndex
+ *  *   Use the _GetVirtualIndex_ function to determine which frame of the animation we should be on based on the current time and playback speed. 
+ *  *   If the animation is looping
+ *      *   Make sure the frame index goes back to 0 if it reaches the last frame.
+ *  *   Otherwise
+ *      *   Make sure the frame index does not go past the last frame.
+ *  *   If the _isDone _param is not null
+ *      *   Set its value based on the current frame index. (Should be **true** if the current frame is the last frame.)
+ *  *   Return the actual frame index the sprite should use by using by adding the starting frame to the frame index.  
+ */
 unsigned Animation::GetCurrentFrameIndex(float dt, float playbackSpeed, bool looping, bool *isDone) const
 {
+	int indx = GetVirtualIndex(dt, playbackSpeed);
+	if (looping && indx)
+		indx = 0;
+	else
+		index = index % frameCount;
+	if (isDone != nullptr && indx == frameCount)
+		isDone = true;
 	return 0;
 }
 
@@ -61,10 +78,22 @@ const std::string &Animation::GetName() const
 // Get the sprite source used by this animation.
 const SpriteSource *Animation::GetSpriteSource() const
 {
-	return nullptr;
+	return spriteSource;
 }
 
+/* 3.  GetVirtualIndex
+    *   _\[NOTE: The goal of this function is to figure out which frame we should be on based on how much time has passed. We will do this by adding the change in
+	* 	time to an accumulator. If the accumulator accumulates enough time, we advance the frame.\]_
+    *   Add time to the accumulator (_dt _times _playbackSpeed)._
+    *   While the accumulator meets or exceeds the frame duration
+        *   Increase the current frame index.
+        *   Decrease the accumulator using _dt._
+    *   Return the current frame.
+*/
 unsigned Animation::GetVirtualIndex(float dt, float playbackSpeed) const
 {
-	return 0;
+	accumulator = dt * playbackSpeed;
+	while (accumulator >= frameDuration)
+		++currentVirtualFrame, accumulator -= frameDuration;
+	return currentVirtualFrame;
 }
