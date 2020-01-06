4defc830bc04e42886d4f7840d62c839faff5cc8
PowerUser64 <blakelysnorth@gmail.com>
2020-01-06 15:12:37 -0800

    takes fractions as an input, does math, and fractions out

---
+++ FractionMath.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#define pr(thing) std::cout << thing
+#define prn(thing) std::cout << thing << std::endl
+
+// 48 == '0'
+
+// changes text to a string
+int *Decode(char *input)
+{
+
+}
+
+// changes values to text
+char *Encode(int *output)
+{
+
+}
+
+int main()
+{
+    char *input = new char[64];
+    // int *decoded = new decoded[];
+    pr("testing text decode");
+    input[0] = '4';
+    Decode(input);
+}
